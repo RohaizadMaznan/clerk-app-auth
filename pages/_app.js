22b9f770f4e8ae0a5d3dd5e80bba33aa4e006600
Rohaizad Maznan <rohaizadmaznan@gmail.com>
2021-09-29 23:36:04 +0800

    Added auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,35 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import {
+  ClerkProvider,
+  RedirectToSignIn,
+  SignedIn,
+  SignedOut,
+} from "@clerk/clerk-react";
+import { useRouter } from "next/router";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+const publicPages = ["/sign-in/[[...index]]", "/sign-up/[[...index]]"];
 
-export default MyApp
+const MyApp = ({ Component, pageProps }) => {
+  const router = useRouter();
+  return (
+    <ClerkProvider
+      frontendApi={process.env.NEXT_PUBLIC_CLERK_FRONTEND_API}
+      navigate={(to) => router.push(to)}
+    >
+      {publicPages.includes(router.pathname) ? (
+        <Component {...pageProps} />
+      ) : (
+        <>
+          <SignedIn>
+            <Component {...pageProps} />
+          </SignedIn>
+          <SignedOut>
+            <RedirectToSignIn />
+          </SignedOut>
+        </>
+      )}
+    </ClerkProvider>
+  );
+};
+
+export default MyApp;
